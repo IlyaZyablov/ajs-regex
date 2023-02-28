a62d4b5213dca5bc103a09112d56684ceb3993ab
KennyHellwood <ilya.zyablov@mail.ru>
2023-02-28 16:04:44 +0300

    validate update

--- src/__tests__/validate.test.js
+++ src/__tests__/validate.test.js
@@ -6,6 +6,9 @@ test.each([
   ['IlyaZyablov_', false],
   ['Ilya123Zyablov', false],
   ['5_IlyaZyablov', false],
+  ['ИльяЗяблов', false],
+  ['IlyaЗяблов', false],
+  ['Ilya_Zyablov', true],
 ])('Validate status for name %s', (name, status) => {
   const data = Validate.validateUsername(name);
   expect(data).toEqual(status);
--- src/validate.js
+++ src/validate.js
@@ -1,13 +1,23 @@
 export default class Validate {
   static validateUsername(name) {
-    // символы, цифры и пробелы в начале и в конце
-    const regexpFirst = /^(?!\d{1}|\s|_|-).+[A-Za-z0-9_-].(?<!\d{1}|\s|_|-)$/;
-    // 3 цифры подряд
-    const regexpSecond = /\d{3}/g;
-    if (name.match(regexpSecond)) {
+    // латинские буквы
+    const allowLatin = /[a-zA-Z]+/g.test(name);
+    // цифры
+    const allowDigits = /[0-9]+/g.test(name);
+    // нижнее подчеркивание и дефис
+    const allowDash = /[_-]+/g.test(name);
+    // запрещены цифры, пробелы, нижнее подчеркивание и дефис в начале
+    const exeptDigitsSpacesDashesBeforeAndAfter = /^(?!\d{1}|\s|_|-).+\w.(?<!\d{1}|\s|_|-)$/;
+    // запрещены 3 цифры подряд
+    const exeptThreeNumbersInRow = /\d{3}/g;
+    // const regexpFirst = /^(?!\d{1}|\s|_|-).+[A-Za-z0-9_-].(?<!\d{1}|\s|_|-)$/;
+    if (name.match(exeptThreeNumbersInRow) || !name.match(exeptDigitsSpacesDashesBeforeAndAfter)) {
       return false;
     }
-    return regexpFirst.test(name);
+    if (!allowLatin && !allowDigits && !allowDash) {
+      return false;
+    }
+    return true;
   }
 }
 
@@ -16,3 +26,6 @@ export default class Validate {
 // console.log(Validate.validateUsername('IlyaZyablov_')); // false
 // console.log(Validate.validateUsername('Ilya123Zyablov')); // false
 // console.log(Validate.validateUsername('5_IlyaZyablov')); // false
+// console.log(Validate.validateUsername('ИльяЗяблов')); // false
+// console.log(Validate.validateUsername('IlyaЗяблов')); // false
+// console.log(Validate.validateUsername('Ilya_Zyablov')); // true
